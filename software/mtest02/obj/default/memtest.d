e6112729d9615f39e9d511c0306eb149eb9630f1
Linux JBL <linuxjbl@gmail.com>
2021-07-04 17:21:20 +0900

    Chap09

--- software/mtest02/obj/default/memtest.d
+++ software/mtest02/obj/default/memtest.d
@@ -1,39 +1,39 @@
-obj/default/memtest.o: memtest.c ../mtest02_bsp//HAL/inc/alt_types.h \
- ../mtest02_bsp//HAL/inc/io.h ../mtest02_bsp//HAL/inc/alt_types.h \
+obj/default/memtest.o: memtest.c ../mtest02_bsp//HAL/inc/sys/alt_stdio.h \
  ../mtest02_bsp/system.h ../mtest02_bsp/linker.h \
- ../mtest02_bsp//HAL/inc/sys/alt_dma.h \
- ../mtest02_bsp//HAL/inc/sys/alt_dma_dev.h \
- ../mtest02_bsp//HAL/inc/priv/alt_dev_llist.h \
- ../mtest02_bsp//HAL/inc/sys/alt_llist.h \
+ ../mtest02_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../mtest02_bsp//HAL/inc/io.h ../mtest02_bsp//HAL/inc/alt_types.h \
+ ../mtest02_bsp//HAL/inc/alt_types.h \
  ../mtest02_bsp//HAL/inc/sys/alt_flash.h \
  ../mtest02_bsp//HAL/inc/sys/alt_flash_types.h \
  ../mtest02_bsp//HAL/inc/sys/alt_flash_dev.h \
+ ../mtest02_bsp//HAL/inc/sys/alt_llist.h \
+ ../mtest02_bsp//HAL/inc/priv/alt_dev_llist.h \
  ../mtest02_bsp//HAL/inc/sys/alt_cache.h \
  ../mtest02_bsp//HAL/inc/sys/alt_flash_dev.h
 
-../mtest02_bsp//HAL/inc/alt_types.h:
-
-../mtest02_bsp//HAL/inc/io.h:
-
-../mtest02_bsp//HAL/inc/alt_types.h:
+../mtest02_bsp//HAL/inc/sys/alt_stdio.h:
 
 ../mtest02_bsp/system.h:
 
 ../mtest02_bsp/linker.h:
 
-../mtest02_bsp//HAL/inc/sys/alt_dma.h:
+../mtest02_bsp//drivers/inc/altera_avalon_pio_regs.h:
 
-../mtest02_bsp//HAL/inc/sys/alt_dma_dev.h:
+../mtest02_bsp//HAL/inc/io.h:
 
-../mtest02_bsp//HAL/inc/priv/alt_dev_llist.h:
+../mtest02_bsp//HAL/inc/alt_types.h:
 
-../mtest02_bsp//HAL/inc/sys/alt_llist.h:
+../mtest02_bsp//HAL/inc/alt_types.h:
 
 ../mtest02_bsp//HAL/inc/sys/alt_flash.h:
 
 ../mtest02_bsp//HAL/inc/sys/alt_flash_types.h:
 
 ../mtest02_bsp//HAL/inc/sys/alt_flash_dev.h:
+
+../mtest02_bsp//HAL/inc/sys/alt_llist.h:
+
+../mtest02_bsp//HAL/inc/priv/alt_dev_llist.h:
 
 ../mtest02_bsp//HAL/inc/sys/alt_cache.h:
 
